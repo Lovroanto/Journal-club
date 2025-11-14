099b485c9cfc73537907d56cf8aad5a6f8d91a70
Your Name <your.email@example.com>
2025-11-14 23:36:47 +0100

    verzija koja je radila prije utaknice

--- minimal.py
+++ minimal.py
@@ -4,7 +4,7 @@ from grobid_preprocessor import preprocess_pdf
 
 pdf_path = "/home/lbarisic/ai_data/Journal_Club/First/summaries/references/articles/2012.08885v5.pdf"
 output_dir = "/home/lbarisic/ai_data/Journal_Club/First/test_article/"
-results = preprocess_pdf(pdf_path, output_dir, chunk_size=4000)
+results = preprocess_pdf(pdf_path, output_dir, chunk_size=5000)
 print("✅ Preprocessing complete!")
 print(f"Title + abstract saved at: {results['title_txt']}")
 print(f"Main chunks: {results['main_chunks']}, Supplementary chunks: {results['supp_chunks']}")
